3c38f648f20110c805cad509eaaee377680548be
Borja Pérez <bdev@pc.local>
2018-06-16 13:23:09 +0200

    search all / search by userId

--- addresses-service/routes/api/address.js
+++ addresses-service/routes/api/address.js
@@ -1,28 +1,41 @@
 const router = require('express').Router();
-const address = require('../../models/address');
+const Address = require('../../models/address');
 
 // retrieve all addresses && retrieve all addresses that match a pattern (i.e. All addresses that contains "Street", or all addresses that contains "Toledo"
 router.get('/', (req, res) => {
-  address.find({}).then((result) => {
-    console.log(result);
-    res.send('retrieve all addresses && retrieve all addresses that match a pattern (i.e. All addresses that contains "Street", or all addresses that contains "Toledo")');
+  let find = {};
+  if (req.query.text) {
+    find = { address: { $regex: `.*${req.query.text}.*` } }
+  }
+
+  Address.find(find, (err, result) => {
+    if (err) {
+      return res.status(400).send(err);
+    }
+    return res.status(200).send(result);
   });
 });
 
 // retrieve all addresses of a user, identified by userId
 router.get('/byUser/:id', (req, res) => {
-  res.send('retrieve all addresses of a user, identified by userId');
-})
+  const userId = req.params.id;
+  Address.find({ userId: userId }, (err, result) => {
+    if (err) {
+      return res.status(400).send(err);
+    }
+    return res.status(200).send(result);
+  });
+});
 
 router.post('/', (req, res) => {
   res.send('create');
 });
 
-router.put('/', (req, res) => {
+router.put('/:id', (req, res) => {
   res.send('update');
 });
 
-router.delete('/', (req, res) => {
+router.delete('/:id', (req, res) => {
   res.send('delete');
 });
 
--- addresses-service/server.js
+++ addresses-service/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 mongoose.connect('mongodb://localhost/gdg-address');
 app.use(require('./routes'));
